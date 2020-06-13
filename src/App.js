fb9c15690e656ebe2fcac2eee140b83c4089b5bd
Bibek Koirala <bibek.high@gmail.com>
2020-06-13 23:42:11 +0530

    React-Todo-APP v-1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div>
+    <h1>Bibek Koirala</h1>
+    <p>My name is Bibek Koirala</p>
+    <p>My hobbies are:</p>
+    <ol>
+      <li>Playing</li>
+      <li>sleeping</li>
+    </ol>
     </div>
   );
 }
